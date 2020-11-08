617180b249145f60052330c9f9e2052ad676b8fe
Marc-Philippe Huget <mphuget@gmail.com>
2020-11-08 13:56:53 +0100

    / and /about are the allowed routes

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('*', (req, res) => {
-  res.send('Express response');
+app.get('/', (req, res) => {
+  res.send('Homepage');
 });
+
+app.get("/about", (req, res) => {
+  res.send("About");
+})
+
 
 app.listen(4000);
 console.log('App server running on port 4000');
